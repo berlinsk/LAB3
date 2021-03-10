4d88114d93a874a97fa2f77b8c953aa92cc7d913
berlinsky <berliniumyar@gmail.com>
2021-03-10 21:31:54 +0200

    First(.gitignore + main.cpp)

---
+++ LAB3/LAB3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LAB3
+//
+//  Created by Берлинский Ярослав Владленович on 10.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
